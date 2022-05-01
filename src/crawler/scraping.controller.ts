15f2aab91cfb4b6cbe440285e58e9e0484e8d364
lmmagalhaes <leo.maga17@gmail.com>
2022-05-01 20:59:41 -0300

    ✨ add UseGuards on scraping Controller

--- src/crawler/scraping.controller.ts
+++ src/crawler/scraping.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/shared/jwt-auth.guard';
 import { ScrapingService } from './scraping.service';
 
 @Controller('scraping')
 export class ScrapingController {
   constructor(private scrapingService: ScrapingService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async scrapping() {
     return this.scrapingService.getScrapping();
