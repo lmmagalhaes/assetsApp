56508911d5c17d4492fed2d51c04a91d62df6187
lmmagalhaes <leo.maga17@gmail.com>
2022-05-02 09:18:54 -0300

    ⚡️ add typeOrmModule forFeature

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../database/models/user.model';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './shared/auth.service';
@@ -7,9 +9,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { jwtConstants } from './shared/constants';
 import { JwtStrategy } from './shared/jwt.strategy';
+import GetUserByEmailService from '../user/get-user-by-email/get-user-email.service';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([User]),
     UserModule,
     PassportModule,
     JwtModule.register({
@@ -18,6 +22,6 @@ import { JwtStrategy } from './shared/jwt.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, LocalStrategy, GetUserByEmailService, JwtStrategy],
 })
 export class AuthModule {}
