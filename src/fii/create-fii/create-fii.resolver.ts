91db7a979a56533064937f520e660bed47a764bd
lmmagalhaes <leo.maga17@gmail.com>
2022-05-01 21:14:42 -0300

    ⚡️ add authGuard

--- src/fii/create-fii/create-fii.resolver.ts
+++ src/fii/create-fii/create-fii.resolver.ts
@@ -1,12 +1,15 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { FII } from 'src/database/models/fii.model';
+import { FII } from '../../database/models/fii.model';
 import CreateFIIInput from './create-fii.input';
 import CreateFIIService from './create-fii.service';
+import { JwtAuthGuard } from '../../auth/shared/jwt-auth.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Resolver(() => FII)
 export default class CreateFIIResolver {
   constructor(private service: CreateFIIService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => FII)
   async createFii(
     @Args('input', { type: () => CreateFIIInput }) input: CreateFIIInput,
