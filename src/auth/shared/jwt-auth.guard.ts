6eb509536b349cb783f3b4d466060e099933b4fc
lmmagalhaes <leo.maga17@gmail.com>
2022-05-02 09:19:34 -0300

    ⚡️ add graphQL code

--- src/auth/shared/jwt-auth.guard.ts
+++ src/auth/shared/jwt-auth.guard.ts
@@ -1,5 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  getRequest(context: ExecutionContext): Request {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req;
+  }
+}
