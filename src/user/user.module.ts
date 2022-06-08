a78512cafbce590d52f00f1df9773e9b1bb630a2
lmmagalhaes <leo.maga17@gmail.com>
2022-06-08 14:48:36 -0300

    ✅ remove all resolvers and includes all controllers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,29 +2,31 @@ import { Module } from '@nestjs/common';
 import { User } from '../database/models/user.model';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import CreateUserService from './create-user/create-user.service';
-import CreateUserResolver from './create-user/create-user.resolver';
 import GetOneUserService from './get-one-user/get-one-user.service';
-import GetOneUserResolver from './get-one-user/get-one-user.resolver';
 import ListUsersService from './list-user/list-user.service';
-import ListUsersResolver from './list-user/list-user.resolver';
 import UpdateUserService from './update-user/update-user.service';
-import UpdateUserResolver from './update-user/update-user.resolver';
 import GetUserByEmailService from './get-user-by-email/get-user-email.service';
-import GetUserByEmailResolver from './get-user-by-email/get-user-email.resolver';
+import CreateUserController from './create-user/create-user.controller';
+import GetOneUserController from './get-one-user/get-one-user.controller';
+import ListUsersController from './list-user/list-user.controller';
+import UpdateUserController from './update-user/update-user.controller';
+import GetUserByEmailController from './get-user-by-email/get-user-email.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
+  controllers: [
+    CreateUserController,
+    GetOneUserController,
+    GetUserByEmailController,
+    ListUsersController,
+    UpdateUserController,
+  ],
   providers: [
     CreateUserService,
-    CreateUserResolver,
     GetOneUserService,
-    GetOneUserResolver,
     GetUserByEmailService,
-    GetUserByEmailResolver,
     ListUsersService,
-    ListUsersResolver,
     UpdateUserService,
-    UpdateUserResolver,
   ],
 })
 export class UserModule {}
