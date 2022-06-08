10dc942ead86267282c118cfc490c65590c341c1
lmmagalhaes <leo.maga17@gmail.com>
2022-06-08 14:44:10 -0300

    ✅ remove all resolvers and includes all controllers

--- src/fii/fii.module.ts
+++ src/fii/fii.module.ts
@@ -1,26 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FII } from '../database/models/fii.model';
-import CreateFIIResolver from './create-fii/create-fii.resolver';
+import CreateFIIController from './create-fii/create-fii.controller';
 import CreateFIIService from './create-fii/create-fii.service';
-import DeleteFIIResolver from './delete-fii/delete-fii.resolver';
+import DeleteFIIController from './delete-fii/delete-fii.controller';
 import DeleteFIIService from './delete-fii/delete-fii.service';
-import ListFIIsResolver from './list-fii/list-fii.resolver';
+import ListFIIsController from './list-fii/list-fii.controller';
 import ListFIIsService from './list-fii/list-fii.service';
-import UpdateFIIResolver from './update-fii/update-fii.resolver';
+import UpdateFIIController from './update-fii/update-fii.controller';
 import UpdateFIIService from './update-fii/update-fii.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([FII])],
+  controllers: [
+    CreateFIIController,
+    DeleteFIIController,
+    ListFIIsController,
+    UpdateFIIController,
+  ],
   providers: [
     CreateFIIService,
-    CreateFIIResolver,
     DeleteFIIService,
-    DeleteFIIResolver,
     ListFIIsService,
-    ListFIIsResolver,
     UpdateFIIService,
-    UpdateFIIResolver,
   ],
 })
 export default class FiiModule {}
